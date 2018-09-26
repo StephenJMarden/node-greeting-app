e7ca125361ac0871ee35ae99f16b39e95d8e9759
StephenJMarden <stephen.j.marden@gmail.com>
2018-09-26 17:19:58 -0400

    Checkpoint complete

---
+++ index.js
@@ -0,0 +1,20 @@
+const   greetings = require('./greeting.js'),
+        slangGreetings = require('./slang-greeting.js');
+
+// Hello
+greetings.greetingEnglish();
+// Konichiwa
+greetings.greetingJapanese();
+// Hola
+greetings.greetingSpanish();
+// Halló
+greetings.greetingIcelandic();
+
+// What's up
+slangGreetings.slangEnglish();
+// Dō shita no
+slangGreetings.slangJapanese();
+// Que pasa
+slangGreetings.slangSpanish();
+// Hvað er að frétta
+slangGreetings.slangIcelandic();
---
+++ slang-greeting.js
@@ -0,0 +1,15 @@
+exports.slangEnglish = () => {
+    console.log("What's up");
+};
+
+exports.slangJapanese = () => {
+    console.log("Dō shita no");
+};
+
+exports.slangSpanish = () => {
+    console.log("Que pasa");
+};
+
+exports.slangIcelandic = () => {
+    console.log("Hvað er að frétta");
+};
